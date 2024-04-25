dcc3b5d80f6912bde4300b71fe80d4c358dc083e
miasumon <miasumon.ru@gmail.com>
2024-04-25 10:39:09 +0600

    update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000
 
 
 // middleware
-app.use(cors())
+app.use(cors({origin:["http://localhost:5000", "https://simple-crud-server-lemon.vercel.app"]}))
 app.use(express.json())
 
 
